53e3018cc9df7aafa8ca3ade8fa436604b1f29ae
Sungju Kwon <sungju.kwon@gmail.com>
2024-08-14 06:51:17 +0000

    Shows audit info in color

--- source/cmds/auditinfo.py
+++ source/cmds/auditinfo.py
@@ -1,6 +1,8 @@
 import sys
 import time
 
+import ansicolor
+
 def description():
     return "Checking audit related information"
 
@@ -13,6 +15,33 @@ def get_command_info():
     return "audit", run_auditinfo
 
 
+COLOR_ONE   = ansicolor.get_color(ansicolor.YELLOW)
+COLOR_TWO   = ansicolor.get_color(ansicolor.GREEN)
+COLOR_THREE = ansicolor.get_color(ansicolor.RED)
+COLOR_FOUR  = ansicolor.get_color(ansicolor.CYAN)
+COLOR_RESET = ansicolor.get_color(ansicolor.RESET)
+
+keyword_color = {
+        "type=" : COLOR_ONE,
+        "syscall=" : COLOR_ONE,
+        "SYSCALL=" : COLOR_ONE,
+        "msg=audit(" : COLOR_TWO,
+        "key=" : COLOR_THREE,
+        "comm=" : COLOR_FOUR,
+}
+
+def get_colored_line(line):
+    result_str = ""
+    words = line.split()
+    for word in words:
+        for keyword in keyword_color:
+            if word.startswith(keyword):
+                word = keyword_color[keyword] + word + COLOR_RESET
+        result_str = result_str + word + " "
+
+    return result_str.strip() + "\n"
+
+
 def read_audit_log(audit_path):
     result_str = ""
     with open(audit_path) as f:
@@ -20,10 +49,11 @@ def read_audit_log(audit_path):
         for line in lines:
             words = line.split()
             epoch = words[1][len("msg=audit("):-2]
-            localtime = time.ctime(float(epoch.split(':')[0]))
-            result_str = result_str + line.replace(epoch, localtime)
+            localtime = time.ctime(float(epoch.split(':')[0])).replace(' ', '_')
+            line = line.replace(epoch, localtime)
+            result_str = result_str + get_colored_line(line)
 
-    return result_str
+    return result_str.strip()
 
 
 def run_auditinfo(input_str, env_vars, show_help=False):
