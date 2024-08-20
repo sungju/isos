db30dde89de4a6967d14d89e4111d734a4be06f5
Sungju Kwon <sungju.kwon@gmail.com>
2024-08-20 04:00:14 +0000

    Audit can be interruptible and pipe aware

--- source/cmds/auditinfo.py
+++ source/cmds/auditinfo.py
@@ -3,6 +3,15 @@ import time
 
 import ansicolor
 
+import signal
+
+stop_cmd = False
+
+def ctrl_c_handler(signum, frame):
+    global stop_cmd
+    stop_cmd = True
+
+
 def description():
     return "Checking audit related information"
 
@@ -21,16 +30,37 @@ COLOR_THREE = ansicolor.get_color(ansicolor.RED)
 COLOR_FOUR  = ansicolor.get_color(ansicolor.CYAN)
 COLOR_RESET = ansicolor.get_color(ansicolor.RESET)
 
-keyword_color = {
-        "type=" : COLOR_ONE,
-        "syscall=" : COLOR_ONE,
-        "SYSCALL=" : COLOR_ONE,
-        "msg=audit(" : COLOR_TWO,
-        "key=" : COLOR_THREE,
-        "comm=" : COLOR_FOUR,
-}
+keyword_color = { }
+
+def set_color_table(no_pipe):
+    global COLOR_ONE, COLOR_TWO, COLOR_THREE
+    global COLOR_FOUR, COLOR_RESET
+    global keyword_color
+
+    if no_pipe:
+        COLOR_ONE   = ansicolor.get_color(ansicolor.YELLOW)
+        COLOR_TWO   = ansicolor.get_color(ansicolor.GREEN)
+        COLOR_THREE = ansicolor.get_color(ansicolor.RED)
+        COLOR_FOUR  = ansicolor.get_color(ansicolor.CYAN)
+        COLOR_RESET = ansicolor.get_color(ansicolor.RESET)
+
+        keyword_color = {
+                "type=" : COLOR_ONE,
+                "syscall=" : COLOR_ONE,
+                "SYSCALL=" : COLOR_ONE,
+                "msg=audit(" : COLOR_TWO,
+                "key=" : COLOR_THREE,
+                "comm=" : COLOR_FOUR,
+        }
+    else:
+        COLOR_ONE = COLOR_TWO = COLOR_THREE = ""
+        COLOR_FOUR = COLOR_RESET = ""
+        keyword_color = {}
+
 
 def get_colored_line(line):
+    global keyword_color
+
     result_str = ""
     words = line.split()
     for word in words:
@@ -42,24 +72,41 @@ def get_colored_line(line):
     return result_str.strip() + "\n"
 
 
-def read_audit_log(audit_path):
+def read_audit_log(audit_path, no_pipe):
+    global stop_cmd
+
+    set_color_table(no_pipe)
     result_str = ""
-    with open(audit_path) as f:
-        lines = f.readlines()
-        for line in lines:
-            words = line.split()
-            epoch = words[1][len("msg=audit("):-2]
-            localtime = time.ctime(float(epoch.split(':')[0]))
-            line = get_colored_line(line)
-            line = line.replace(epoch, localtime)
-            result_str = result_str + line
+    try:
+        with open(audit_path) as f:
+            lines = f.readlines()
+            for line in lines:
+                if stop_cmd:
+                    break
+                words = line.split()
+                epoch = words[1][len("msg=audit("):-2]
+                localtime = time.ctime(float(epoch.split(':')[0]))
+                line = get_colored_line(line)
+                line = line.replace(epoch, localtime)
+                if no_pipe:
+                    print(line.strip())
+                else:
+                    result_str = result_str + line
+    except:
+        result_str = ""
 
     return result_str.strip()
 
 
 def run_auditinfo(input_str, env_vars, show_help=False, no_pipe=True):
+    global stop_cmd
+
     if show_help == True:
         return description()
 
-    result_str = read_audit_log(env_vars["sos_home"] + "/var/log/audit/audit.log")
+    stop_cmd = False
+    orig_handler = signal.signal(signal.SIGINT, ctrl_c_handler)
+
+    result_str = read_audit_log(env_vars["sos_home"] + "/var/log/audit/audit.log", no_pipe)
+    signal.signal(signal.SIGINT, orig_handler)
     return result_str
