13d282d6f34b5d20f6fb79a4c349d8897d31392e
Sungju Kwon <sungju.kwon@gmail.com>
2025-04-22 03:30:41 +0000

    RULE for migration_entry_wait_on_locked() bug

---
+++ source/rules/migration_entry_wait_on_locked_bug.py
@@ -0,0 +1,73 @@
+import sys
+import ntpath
+import operator
+import math
+
+def is_major():
+    return True
+
+
+def description():
+    return "RHEL 8.8: hung_task_timeout_secs at migration_entry_wait_on_locked"
+
+
+def add_rule(sysinfo):
+    if sysinfo is None or "RELEASE" not in sysinfo:
+        return True
+
+    release = sysinfo["RELEASE"]
+    if ("el8") in release:
+        return True
+
+    return False
+
+
+def run_rule(basic_data):
+    env_vars = basic_data["env_vars"]
+    sos_home = env_vars["sos_home"]
+    try:
+        if basic_data == None:
+            return None
+        else:
+            log_string = basic_data["log_str"]
+        migration_entry_wait_on_locked = log_string.find("migration_entry_wait_on_locked+0x")
+        hung_task_msg = log_string.rfind("INFO: task ")
+
+        if hung_task_msg < 0 or migration_entry_wait_on_locked < 0:
+            return None
+
+        hung_task_msg = log_string.rfind('\n', 0, hung_task_msg)
+        msg_str = ""
+        for line in log_string[hung_task_msg:].strip().splitlines():
+            if " kernel: " not in line:
+                break
+            msg_str = msg_str + line + "\n"
+
+        msg_str = msg_str.strip()
+        result_dict = {}
+        result_dict["TITLE"] = "system hang in migration_entry_wait_on_locked()"\
+                               " bug\n\tdetected by %s" % \
+                                ntpath.basename(__file__)
+        result_dict["MSG"] = msg_str
+        result_dict["KCS_TITLE"] = "RHEL 8.8: hung_task_timeout_secs at " \
+                                    "migration_entry_wait_on_locked"
+        result_dict["KCS_URL"] = "https://access.redhat.com/solutions/7014646"
+        result_dict["RESOLUTION"] = "Please upgrade kernel as specified in the KCS"
+        result_dict["KERNELS"] = { "kernel-4.18.0-477.13.1.el8_8",
+                                  "kernel-4.18.0-372.96.1.el8_6.x86_64" }
+
+        return [result_dict]
+    except Exception as e:
+        print(e)
+        return None
+
+
+
+def migration_entry_wait_on_locked_bug():
+    import pprint
+    pp = pprint.PrettyPrinter(indent=0, width=180)
+    pp.pprint(run_rule(None))
+
+
+if ( __name__ == '__main__'):
+    migration_entry_wait_on_locked_bug()
