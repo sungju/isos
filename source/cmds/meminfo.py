80a71d30370a3825fcf7e8d8a2c334faffbccbb0
Sungju Kwon <sungju.kwon@gmail.com>
2026-03-04 12:19:19 +1000

    Shows half filled bar as well
    Also fixed incorrect total memory calcuation

--- source/cmds/meminfo.py
+++ source/cmds/meminfo.py
@@ -38,8 +38,8 @@ def get_system_total_memory_kb():
     Returns:
         int: Total memory in KB, or 0 if unable to determine
     """
+    global sos_home
     try:
-        sos_home = get_main().sos_home
         with open(sos_home + "/proc/meminfo") as f:
             for line in f:
                 if "MemTotal:" in line:
@@ -218,8 +218,14 @@ def get_memory_bar(percentage, width=20, no_pipe=True):
     # Use block characters for visual appeal
     filled_char = '█'
     empty_char = '░'
+    half_char = '▌'  # Half-filled character for very small percentages
 
-    bar = '[' + (filled_char * filled_width) + (empty_char * empty_width) + ']'
+    # Show minimal indicator for very small but non-zero percentages
+    if percentage > 0 and filled_width == 0:
+        # Use half-filled character to indicate minimal usage
+        bar = '[' + half_char + (empty_char * (width - 1)) + ']'
+    else:
+        bar = '[' + (filled_char * filled_width) + (empty_char * empty_width) + ']'
 
     # Add color coding based on usage level
     if no_pipe:
